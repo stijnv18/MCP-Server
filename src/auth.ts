809e4ed00280e3a8d298918f4f465d44baa87a6e
Stijn Verheyden <Stijn.Verheyden@kaneka.be>
2025-09-17 11:42:59 +0200

    Fix auth for powerapps

--- src/auth.ts
+++ src/auth.ts
@@ -3,7 +3,7 @@ import { apiKey } from './config.js';
 import * as Sentry from '@sentry/node';
 
 export function checkAuth(req: IncomingMessage, res: ServerResponse): boolean {
-  const authHeader = req.headers['authorization'];
+  const authHeader = req.headers['token'] || req.headers['authorization']; //token is custom header cause powerapps is stupid "Authorization" is standard
   if (!authHeader || authHeader !== `Bearer ${apiKey}`) {
     Sentry.captureMessage('Unauthorized access attempt', 'warning');
     res.writeHead(401, { 'Content-Type': 'application/json' });
